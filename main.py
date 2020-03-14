b0d7ddd34998be337f3a145ba1d860af81d7fc7c
vsharmanyc <vsharma.nyc@gmail.com>
2020-03-14 19:17:04 -0400

    handle case where both strings are not the same length

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 
 def isMapping(s1, s2):
+	if len(s1) != len(s2):
+		return False
+
 	mapping = {}
 	for i in range(len(s1)):
 		if mapping.setdefault(s1[i],s2[i]) != s2[i]:
 			print("{ " + s1[i] +  " , "  + mapping[s1[i]] + ' }  ... ' + s2[i])
 			return False
+			
 	return True
 
 
